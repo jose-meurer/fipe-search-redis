726f2b8dcaceefb652d634815d8cb954d9fcec14
Jose Meurer <josewmeurer@gmail.com>
2026-04-26 16:28:03 -0300

    Geração do projeto com spring initializr com pacotes Web, Jpa e Redis

---
+++ src/main/java/com/josemeurer/fipeSearchRedis/FipeSearchRedisApplication.java
@@ -0,0 +1,13 @@
+package com.josemeurer.fipeSearchRedis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FipeSearchRedisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FipeSearchRedisApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/josemeurer/fipeSearchRedis/FipeSearchRedisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.josemeurer.fipeSearchRedis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FipeSearchRedisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
